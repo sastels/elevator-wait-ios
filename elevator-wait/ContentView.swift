27b7f35e74c9b60d5628724eb527cbdd88f1132e
Steve Astels <sastels@gmail.com>
2021-01-19 15:18:02 -0500

    rough in

--- elevator-wait/ContentView.swift
+++ elevator-wait/ContentView.swift
@@ -8,14 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  @ObservedObject var stopWatchManager = StopWatchManager()
+  
+  var timerName = "test"
+  
+  var body: some View {
+    VStack {
+      Spacer()
+      if stopWatchManager.mode != .stopped {
+        let minutes = stopWatchManager.secondsElapsed / 60
+        let seconds = stopWatchManager.secondsElapsed % 60
+        Text("Elapsed: \(String(format: "%02d:%02d", minutes, seconds))")
+        
+      } else {
+        Text("Timer: \(timerName)")
+          .padding()
+      }
+      Spacer()
+      
+      switch stopWatchManager.mode {
+        case .stopped:
+          Button(action: { stopWatchManager.start() }) {
+            Text("Start")
+              .fontWeight(.bold)
+              .font(.title2)
+              .padding()
+              .background(Color.purple)
+              .cornerRadius(40)
+              .foregroundColor(.white)
+          }
+        case .running:
+          Button(action: { stopWatchManager.pause() }) {
+            Text("Pause")
+              .fontWeight(.bold)
+              .font(.title2)
+              .padding()
+              .background(Color.purple)
+              .cornerRadius(40)
+              .foregroundColor(.white)
+          }
+        case .paused:
+          VStack {
+            Spacer()
+            HStack {
+              Button(action: { stopWatchManager.start() }) {
+                Text("Continue")
+                  .fontWeight(.bold)
+                  .font(.title2)
+                  .padding()
+                  .background(Color.purple)
+                  .cornerRadius(40)
+                  .foregroundColor(.white)
+              }
+              Spacer()
+              Button(action: {}) {
+                Text("Submit")
+                  .fontWeight(.bold)
+                  .font(.title2)
+                  .padding()
+                  .background(Color.purple)
+                  .cornerRadius(40)
+                  .foregroundColor(.white)
+              }
+            }
+            Spacer()
+            Button(action: { stopWatchManager.stop() }) {
+              Text("Reset")
+                .fontWeight(.bold)
+                .font(.title2)
+                .padding()
+                .background(Color.red)
+                .cornerRadius(40)
+                .foregroundColor(.white)
+            }
+            Spacer()
+          }
+      }
+      
+      Spacer()
+    }.padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    let c1 = ContentView()
+    let c2 = ContentView()
+    let c3 = ContentView()
+    c2.stopWatchManager.mode = .running
+    c3.stopWatchManager.mode = .paused
+    return
+      Group {
+        c1
+        c2
+        c3
+      }
+  }
 }
---
+++ elevator-wait/StopWatchManager.swift
@@ -0,0 +1,42 @@
+//
+//  StopWatchManager.swift
+//  elevator-wait
+//
+//  Created by Stephen Astels on 2021-01-19.
+//
+// from https://blckbirds.com/post/creating-a-simple-stopwatch-in-swiftui/
+
+import Foundation
+
+import SwiftUI
+enum StopWatchMode {
+  case running
+  case stopped
+  case paused
+}
+
+class StopWatchManager: ObservableObject {
+  @Published var secondsElapsed = 0
+  @Published var mode: StopWatchMode = .stopped
+
+  var timer = Timer()
+
+  func start() {
+    print("Timer started!")
+    mode = .running
+    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+      self.secondsElapsed += 1
+    }
+  }
+
+  func pause() {
+    timer.invalidate()
+    mode = .paused
+  }
+
+  func stop() {
+    timer.invalidate()
+    secondsElapsed = 0
+    mode = .stopped
+  }
+}
