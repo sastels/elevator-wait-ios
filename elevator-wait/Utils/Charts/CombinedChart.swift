b0c6792a07b0c8a6fb40aea9858de4f6e5a921dd
Steve Astels <sastels@gmail.com>
2021-02-04 12:56:00 -0500

    use a median

--- elevator-wait/Utils/Charts/CombinedChart.swift
+++ elevator-wait/Utils/Charts/CombinedChart.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct CombinedChart: UIViewRepresentable {
   var entries: [ChartDataEntry]
   var chart = CombinedChartView()
-  
-  // this func is required to conform to UIViewRepresentable protocol
+
   func makeUIView(context: Context) -> CombinedChartView {
     let leftAxis = chart.leftAxis
     leftAxis.axisMinimum = 0
@@ -45,14 +44,35 @@ struct CombinedChart: UIViewRepresentable {
     return data
   }
 
+  func medianY(x: Double) -> Double {
+    let yValues = entries
+      .filter { data in x - 10 < data.x && data.x < x + 10 }
+      .map { $0.y }
+
+    let median = yValues.isEmpty ? -666.0 : yValues.sorted(by: <)[yValues.count / 2]
+    return median
+  }
+
   func lineData() -> LineChartData {
-    
+    var xMin = self.entries.isEmpty ? 0 : self.entries[0].x
+    var xMax = xMin
+    for data in self.entries {
+      if data.x < xMin {
+        xMin = data.x
+      } else if data.x > xMax {
+        xMax = data.x
+      }
+    }
+    let entries = stride(from: Double(xMin), to: Double(xMax), by: 1.0).map { (i) -> ChartDataEntry in
+      ChartDataEntry(x: i, y: medianY(x: i))
+    }.filter { data in data.y > 0 }
+
     let set = LineChartDataSet(entries: entries, label: "Line DataSet")
     set.setColor(UIColor.black)
     set.lineWidth = 2.5
     set.circleRadius = 0
     set.circleHoleRadius = 0
-    set.mode = .cubicBezier
+//    set.mode = .cubicBezier
     set.drawValuesEnabled = false
     set.axisDependency = .left
 
@@ -68,6 +88,6 @@ struct CombinedChart_Previews: PreviewProvider {
       ChartDataEntry(x: Double(i) + 0.5, y: Double(i) * 1.0 + Double(arc4random_uniform(15) + 5))
     }
 
-    CombinedChart(entries:entries)
+    CombinedChart(entries: entries)
   }
 }
