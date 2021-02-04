d5a15436f284364ae20de0f2d930b1be0febf5eb
Steve Astels <sastels@gmail.com>
2021-02-04 16:25:25 -0500

    fixup line

--- elevator-wait/Utils/Charts/CombinedChart.swift
+++ elevator-wait/Utils/Charts/CombinedChart.swift
@@ -8,6 +8,31 @@
 import Charts
 import SwiftUI
 
+/* web app
+ const INTERVAL_LENGTH = 30.0 / 60; // in hours
+ const INTERVAL_SPACING = 30.0 / 60; // in hours
+
+ const smoothedData = data => {
+ let smoothedTimes = Array(24 / INTERVAL_SPACING)
+   .fill()
+   .map((x, i) => i * INTERVAL_SPACING);
+
+ let smoothedData = smoothedTimes.map(t => {
+   return [
+     t,
+     data.filter(pt => Math.abs(t - pt[0]) < INTERVAL_LENGTH / 2.0)
+   ];
+ });
+ smoothedData = smoothedData.filter(pt => pt[1].length > 1);
+ smoothedData = smoothedData.map(pt => [
+   pt[0],
+   pt[1].reduce((total, pt) => total + pt[1], 0) / pt[1].length
+ ]);
+ return smoothedData;
+ };
+
+ */
+
 struct CombinedChart: UIViewRepresentable {
   var entries: [ChartDataEntry]
   var chart = CombinedChartView()
@@ -39,17 +64,17 @@ struct CombinedChart: UIViewRepresentable {
     let dataSet = ScatterChartDataSet(entries: entries, label: "DS 1")
     dataSet.setScatterShape(.circle)
     dataSet.colors = [NSUIColor.blue]
-    dataSet.label = "My Data"
+    dataSet.label = "Times"
     data.addDataSet(dataSet)
     return data
   }
 
   func medianY(x: Double) -> Double {
     let yValues = entries
-      .filter { data in x - 10 < data.x && data.x < x + 10 }
+      .filter { data in abs(data.x - x) < 0.5 }
       .map { $0.y }
 
-    let median = yValues.isEmpty ? -666.0 : yValues.sorted(by: <)[yValues.count / 2]
+    let median = yValues.count < 3 ? -666.0 : yValues.sorted(by: <)[yValues.count / 2]
     return median
   }
 
@@ -63,16 +88,16 @@ struct CombinedChart: UIViewRepresentable {
         xMax = data.x
       }
     }
-    let entries = stride(from: Double(xMin), to: Double(xMax), by: 1.0).map { (i) -> ChartDataEntry in
+    let entries = stride(from: Double(xMin), to: Double(xMax), by: 0.5).map { (i) -> ChartDataEntry in
       ChartDataEntry(x: i, y: medianY(x: i))
     }.filter { data in data.y > 0 }
 
-    let set = LineChartDataSet(entries: entries, label: "Line DataSet")
+    let set = LineChartDataSet(entries: entries, label: "Median")
     set.setColor(UIColor.black)
     set.lineWidth = 2.5
     set.circleRadius = 0
     set.circleHoleRadius = 0
-//    set.mode = .cubicBezier
+    set.mode = .cubicBezier
     set.drawValuesEnabled = false
     set.axisDependency = .left
 
@@ -84,9 +109,10 @@ struct CombinedChart: UIViewRepresentable {
 
 struct CombinedChart_Previews: PreviewProvider {
   static var previews: some View {
-    let entries = (0..<40).map { (i) -> ChartDataEntry in
-      ChartDataEntry(x: Double(i) + 0.5, y: Double(i) * 1.0 + Double(arc4random_uniform(15) + 5))
-    }
+    let entries = stride(from: 0.0, to: 24.0, by: 0.4)
+      .map { (i) -> ChartDataEntry in
+        ChartDataEntry(x: i + 0.5, y: i * 1.0 + Double(arc4random_uniform(15) + 5))
+      }
 
     CombinedChart(entries: entries)
   }
